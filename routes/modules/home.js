7f34336a5c5e8e0c3faf16998dea5146bfd564b0
etandmouse <etandmouse@gmail.copm>
2023-03-26 15:37:01 +0800

    feat: add sort restaurant when search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -14,11 +14,17 @@ router.get('/', (req, res) => {
 //search restaurants
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword
+  const sort = req.query.sort
+  const sortBy = sort.split("-")
+  const Type = sortBy[0]
+  const order = sortBy[1]
+  console.log(sort)
   Restaurant.find()
     .lean()
+    .sort({ [Type]: order })
     .then(restaurant => {
       let restaurants = restaurant.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLocaleLowerCase()) || restaurant.category.includes(keyword))
-      return res.render('index', { restaurants, keyword })
+      return res.render('index', { restaurants, keyword, sort })
     })
     .catch(error => console.error(error))
 })
