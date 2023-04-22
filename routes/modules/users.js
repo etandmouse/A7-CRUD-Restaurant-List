4a5da5295d745a22a103c5156b45ddbec7df5b1c
etandmouse <etandmouse@gmail.copm>
2023-04-22 16:26:05 +0800

    Add email and password to required item in login page. Add fail message with wrong email and password

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -12,6 +12,7 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
+  failureMessage: true,
   failureRedirect: '/users/login'
 }))
 
